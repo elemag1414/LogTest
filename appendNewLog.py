adb2f83c2af353f18b8643f1b01fd7bb54f385a9
elemag1414 <elemag1414@gmail.com>
2023-06-02 23:21:10 +0900

    Update appendNewLog.py

--- appendNewLog.py
+++ appendNewLog.py
@@ -14,18 +14,24 @@ def random_string(length):
     return ''.join(random.choice(letters) for i in range(length))
 
 # 랜덤 문자열을 history.log 파일에 추가하는 함수
-def append_random_string_to_file(repo, path, message, branch):
+def append_random_string_to_file(repo, path, message, epoch, accuracy, branch):
     contents = repo.get_contents(path, ref=branch)
     now = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
-    newMSG = f'Append New MSG {now}: {random_string(10)}'
+    newMSG = f'** @{now}: \n\t[Epoch {epoch:03d}] eval_accu: {accuracy/10:.1f}%'
     print(newMSG)
-    # repo.update_file(contents.path, message, contents.decoded_content.decode("utf8") + newMSG + '\n', contents.sha, branch=branch)
     repo.update_file(contents.path, message, contents.decoded_content.decode("utf8") + newMSG + '\n', contents.sha, branch=branch)
 
 # GitHub 레포지토리 설정
 repo = g.get_user().get_repo("your_repo_name")
-
+epoch = 1
 # 무한루프를 돌면서 일정 시간마다 랜덤 문자열을 history.log 파일에 추가
 while True:
-    append_random_string_to_file(repo, "history.log", "Add random string", "main")
+
+    accuracy = random.randint(550, 1000)
+    try:
+      append_random_string_to_file(repo, "history.log", "Add new message", epoch, accuracy, "main")
+    except:
+      now = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+      print(f'Fail to update log! ({now})')
     time.sleep(120)  # 120초마다 실행
+    epoch += 1
