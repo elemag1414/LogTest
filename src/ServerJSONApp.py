f89be4a4eacd999e0f8c67519e6578522691dbeb
elemag1414 <elemag1414@gmail.com>
2023-06-11 13:02:49 +0900

    Update ServerJSONApp.py

--- src/ServerJSONApp.py
+++ src/ServerJSONApp.py
@@ -58,32 +58,6 @@ def update_whole_logs():
 
   return jsonify({"result": "OK"})
 
-# @app.route('/data/<req_data>', methods=['GET'])
-# def get_home_data(req_data):
-#   if req_data == 'home_data':
-#     print('fetch request home_data received')
-#     return jsonify(home_data)
-#   elif req_data == 'electron_data':
-#     print('fetch request for electron_data received')
-#     return jsonify(electron_data)
-#   else:
-#     print('Unknown Request data type')
-#     return ""
-
-# @app.route('/reload/<req_data>', methods=['GET'])
-# def reload_data(req_data):
-#   global home_data, electron_data
-#   if req_data == 'home_data':
-#     print('Reload home_data...')
-#     home_data = convert_log_new(home_log_file)
-#     return jsonify(home_data)
-#   elif req_data == 'electron_data':
-#     print('Reload home_data...')
-#     electron_data = convert_log_new(electron_log_file)
-#     return jsonify(electron_data)
-#   else:
-#     print('Unknown Request data type')
-#     return ""
 
 if __name__ == '__main__':
 
